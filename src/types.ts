67dbe9784a893fef3d2a98bdc0320967d74c0e9e
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-07-09 20:50:12 +0300

    adding BaseEntry types and extending BaseEntry to create other entry type

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,62 @@
-export interface Diagnosis {
-  code: string;
-  name: string;
-  latin?: string;
+export interface Diagnosis{
+  code:string;
+  name:string;
+  latin?:string
 }
 
-export enum Gender {
-  Male = 'male',
-  Female = 'female',
-  Other = 'other'
+export enum Gender{
+  Male='male',
+  Female='female' ,
+  Other='other'
+} 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
 }
 
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry{
+  type:"Hospital";
+  discharge:{
+    date:string;
+    criteria:string;
+  }
+}
+interface OccupationalHealthcareEntry extends BaseEntry{
+  type:"OccupationalHealthcare";
+  employerName:string;
+  sickLeave?:{
+    startDate:string;
+    endDate:string;
+  }
+}
+
+export type Entry=HealthCheckEntry | HospitalEntry | OccupationalHealthcareEntry;
 
 export interface Patient {
-  id: string;
-  name: string;
-  occupation: string;
-  gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
-  
+  id:string;
+  name:string;
+  dateOfBirth?:string;
+  ssn:string;
+  gender:Gender;
+  occupation:string;
+  entries:Entry[];
 }
 
-export type PatientFormValues = Omit<Patient, 'id' | "entries" >;
+export type PatientFormValues = Omit<Patient, 'id' | 'entries' >;
